82e01b967f88c1eb5d9726d49fed6ae97c12ae13
Ícaro Hoff <icarohoff@gmail.com>
2018-04-21 14:17:32 -0300

    Fix device string updates and update package-lock.json

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.put("/api/devices/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(DEVICES_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+  db.collection(DEVICES_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, { $set: updateDoc }, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to update device.");
     } else {
