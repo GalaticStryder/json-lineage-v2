381a82d842c3d16b11543a4cbc4ea4fed95c3d4d
Ícaro Hoff <icarohoff@gmail.com>
2018-04-24 11:23:14 -0300

    Add resources page example

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require("express");
+var path = require('path');
 var app = require('./src/app');
 
 // Create a link to the Angular build directory.
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set 'res' route to publish our public resources.
+app.use('/res', express.static(path.join(__dirname, 'res')));
+
+// Redirect to root when trying to get the resources folder itself.
+app.get('/res', function(req, res) {
+  res.redirect('/');
+});
